0304a640ae28d1b2570eee64239edad3f72d2316
mazamu <506143016@qq.com>
2023-02-04 12:27:23 +0000

    update classroom.yml .keep autograding.json for classroom CI test

--- RustTest/os/src/main.rs
+++ RustTest/os/src/main.rs
@@ -1,6 +1,29 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
+
+use log::*;
+
+#[macro_use]
+mod console;
 mod lang_items;
-// fn main() {
-//     // println!("Hello, world!");
-// }
+mod logging;
+mod sbi;
+
+core::arch::global_asm!(include_str!("entry.asm"));
+
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { ( a as *mut u8).write_volatile(0) }
+    });
+}
